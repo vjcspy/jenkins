db28b595d48ea6dc3b4d2ecf3d5bccf25b59b306
dinhkhoi <mr.vjcspy@gmail.com>
2022-05-23 22:14:39 +0700

    _

---
+++ src/dsl/temp/nodejs-example.groovy
@@ -0,0 +1,21 @@
+job('nodejs-example') {
+    scm {
+        git('https://github.com/vjcspy/jenkins.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs16') // this is the name of the NodeJS installation in
+    // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell('pwd')
+        dir('src/jenkins-docker-demo') {
+            shell('npm install')
+        }
+    }
+}
